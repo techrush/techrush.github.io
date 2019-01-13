ddc6b3e5200126807f1288ff5c75cf34fee0e95e
techrush <46622499+techrush@users.noreply.github.com>
2019-01-13 20:08:22 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,43 +5,60 @@ const menu = document.getElementsByClassName('menu-container')[0];
 
 svgRect.addEventListener("click",()=>{
     menu.classList.remove('menu-closed');
-    // menu.classList.add('openAnimation');
+   
     menu.style.height = "60vh";
 
     console.log('cool');
 });
 menuCloser.addEventListener("click",()=>{
-    // menu.classList.add('openAnimation');
+ 
     menu.classList.add('menu-closed');
 });
-function ani(){
-    document.getElementById('text').className ='animationClass';
-  }
-
-
+// function ani(){
+//     document.getElementById('text').className = 'animationClass';
+//   }
+document.getElementsByClassName('shape')[0].addEventListener('click', ()=>{
+    anime({
+        targets: '.text',
+        // translateX: 250
+        color: '#F7153D'
+      });
+    
+});
+document.getElementsByClassName('svg-wrapper')[0].addEventListener('click',()=>{
+    anime({
+        targets: '.emailPopper',
+        top: ['100vh','70vh']
+    });
+    
+});
+document.getElementById('submit').addEventListener('click', ()=>{
+    anime({
+        targets: '.emailPopper',
+        top: ['70vh','100vh']
+    });
+document.getElementById('text').innerText = "NOTIFIED";
+});
   var countDownDate = new Date("Feb 5, 2019 10:30:00").getTime();
 
-// Update the count down every 1 second
+
 var x = setInterval(function() {
 
-    // Get todays date and time
+
     var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
+
     var distance = countDownDate - now;
-  
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-    // Display the result in the element with id="demo"
+    document.getElementById("days1").innerHTML = days;
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("secs").innerHTML = seconds;
-    // If the count down is finished, write some text 
+
     
   }, 1000);
   var typed = new Typed('.demoTyper', {
