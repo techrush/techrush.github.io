2412e75b6eccc451e71c03731b123c29a80e17ba
techrush <46622499+techrush@users.noreply.github.com>
2019-01-13 16:07:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,3 +14,39 @@ menuCloser.addEventListener("click",()=>{
     // menu.classList.add('openAnimation');
     menu.classList.add('menu-closed');
 });
+function ani(){
+    document.getElementById('text').className ='animationClass';
+  }
+
+
+  var countDownDate = new Date("Feb 5, 2019 10:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("min").innerHTML = minutes;
+    document.getElementById("secs").innerHTML = seconds;
+    // If the count down is finished, write some text 
+    
+  }, 1000);
+  var typed = new Typed('.demoTyper', {
+    strings: ["We will not start without you.. ", "It is going to be lit!"],
+    typeSpeed: 120,
+    loop: true,
+    loopCount: Infinity
+  });
